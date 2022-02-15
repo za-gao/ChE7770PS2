c0ff3749c2f644dfda8f403c02d04c6f838dd98c
za-gao <98918294+za-gao@users.noreply.github.com>
2022-02-15 14:39:07 -0500

    PS2

--- PS2-5440-7770-Ziang_Yujia_Xiaoyu_Yiqi.jl
+++ PS2-5440-7770-Ziang_Yujia_Xiaoyu_Yiqi.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.18.0
+# v0.17.5
 
 using Markdown
 using InteractiveUtils
@@ -11,12 +11,12 @@ md"""
 
 # ╔═╡ 7057c8e4-9e94-4a28-a885-07f5c96ebe39
 html"""
-<p style="font-size:20px;">Ziang Gao, zg276, Yujia Huang, yh945, Xiaoyu Zhang, xz352</br>
+<p style="font-size:20px;">Ziang Gao, zg276, Yujia Huang, yh945, Xiaoyu Zhang, xz352, Yiqi Shao, ys882</br>
 Smith School of Chemical and Biomolecular Engineering, Cornell University, Ithaca NY 14850</p>
 Contribution:</br>
-Ziang Gao</br>
-Yujia Huang</br>
-Xiaoyu Zhang</br>
+Ziang & Yujia worked on the stoichiometric array, figured out v's and b's, and confirmed the balanced array contains all zeros. Then analyze the reaction frequency. </br>
+Xiaoyu & Yiqi worked on the metabolite and reaction connectivity array.</br>
+All contributors worked together to discuss the code.</br>
 """
 
 # ╔═╡ 87a183bc-3857-4189-8103-18c46ff3245d
